fce5241a9585e3e191d88a59dcf4f5fb3c7fc634
Aeoluzz <scherwainm@outlook.com>
2021-07-27 17:13:43 -0400

    my initial commit

--- my_first_cdk_project/my_first_cdk_project_stack.py
+++ my_first_cdk_project/my_first_cdk_project_stack.py
@@ -5,6 +5,7 @@ from aws_cdk import core as cdk
 # with examples from the CDK Developer's Guide, which are in the process of
 # being updated to use `cdk`.  You may delete this import if you don't need it.
 from aws_cdk import core
+from aws_cdk import aws_s3 as _s3
 
 
 class MyFirstCdkProjectStack(cdk.Stack):
@@ -13,3 +14,11 @@ class MyFirstCdkProjectStack(cdk.Stack):
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+
+        _s3.Bucket(
+            self,
+            "myBucketId",
+            bucket_name="myfirstcdkprojectzeus",
+            versioned=True,
+            encryption=_s3.BucketEncryption.KMS_MANAGED
+        )
